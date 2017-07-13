00f23f84353cde7df88f439611755fbd1f35fdff
南琴梨钦定丈夫南琴浪 <nanqinlang@users.noreply.github.com>
2017-07-14 03:22:54 +0800

    1.1.0
    big update.

--- tcp_nanqinlang.sh
+++ tcp_nanqinlang.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
-Green_font="\033[32m" && Green_background="\033[42;37m" && Red_font="\033[31m" && Red_background="\033[41;37m" && Font_suffix="\033[0m"
+Green_font="\033[32m" && Yellow_font="\033[33m" && Red_font="\033[31m" && Font_suffix="\033[0m"
 Info="${Green_font}[Info]${Font_suffix}"
 Success="${Green_font}[Success]${Font_suffix}"
 Error="${Red_font}[Error]${Font_suffix}"
 Failed="${Red_font}[Failed]${Font_suffix}"
 Careful="${Green_font}[Be Careful]${Font_suffix}"
-Install="${Green_font}bash tcp_nanqinlang.sh install${Font_suffix}"
-Start="${Green_font}bash tcp_nanqinlang.sh start${Font_suffix}"
+reboot="${Yellow_font}reboot${Font_suffix}"
+Install="${Yellow_font}bash tcp_nanqinlang.sh install${Font_suffix}"
+Start="${Yellow_font}bash tcp_nanqinlang.sh start${Font_suffix}"
 
-echo -e "${Green_font}
+echo -e "${Yellow_font}
 #========================================================
 # System Required: Debian/Ubuntu
 # Description: tcp_nanqinlang
@@ -38,139 +39,155 @@ check_system(){
     fi
 }
 
-#check kernel version is 4.11.0 or not
-check_deb(){
-	deb_ver=`dpkg -l|grep linux-image | awk '{print $2}' | awk -F '-' '{print $3}' | grep '[4-9].[0-9]*.'`
-	if [[ "${deb_ver}" != "" ]]; then
-		if [[ "${deb_ver}" == "4.11.0" ]]; then
-			echo -e "${Info} version is 4.11.0, continuing"
+#delete image
+del_image(){
+	total_image=`dpkg -l | grep linux-image | awk '{print $2}' | grep -v "${latest_version}" | wc -l`
+	if [ "${total_image}" > "1" ]; then
+		echo -e "${Info} find ${total_image} other kernel, uninstalling"
+		for((integer = 1; integer <= ${total_image}; integer++))
+		do
+			surplus_image=`dpkg -l|grep linux-image | awk '{print $2}' | grep -v "${latest_version}" | head -${integer}`
+			echo -e "${Info} uninstalling ${surplus_image}"
+			apt-get purge -y ${surplus_image}
+			echo -e "${Success} uninstall ${surplus_image} successfully, continuing"
+		done
+		if [ "${total_image}" = "0" ]; then
+			echo -e "${Success} uninstall successfully, continuing"
 		else
-			echo -e "${Error} kernel version is not 4.11.0, please use ${Install}"
+			echo -e "${Failed} uninstall failed, please check!" && exit 1
 		fi
 	else
-	echo -e "${Error} version is not support, please use ${Install}" && exit 1
+	echo -e "${Error} check kernel failed, please check!" && exit 1
 	fi
 }
 
-#delete image
-del_image(){
-	image_total=`dpkg -l | grep linux-image | awk '{print $2}' | grep -v "4.11.0" | wc -l`
-	if [ "${image_total}" > "1" ]; then
-		echo -e "${Info} find ${image_total} other kernel, uninstalling"
-		for((integer = 1; integer <= ${image_total}; integer++))
-		do
-			image_del=`dpkg -l|grep linux-image | awk '{print $2}' | grep -v "4.11.0" | head -${integer}`
-			echo -e "${Info} uninstalling ${image_del}"
-			apt-get purge -y ${image_del}
-			echo -e "${Success} uninstalling ${image_del} successfully, continuing"
-		done
-		if [ "${image_total}" = "0" ]; then
-			echo -e "${Success} uninstalling successfully, continuing"
-		else
-			echo -e "${Failed} uninstalling failed, please check!" && exit 1
-		fi
-	else
-	echo -e "${Error} find a wrong kernel number, please check!" && exit 1
-	fi
-}
 
 #delete headers
 del_headers(){
-	headers_total=`dpkg -l | grep linux-headers | awk '{print $2}' | grep -v "4.11.0" | wc -l`
-	if [ "${headers_total}" > "1" ]; then
-		echo -e "${Info} find ${headers_total} other kernel, uninstalling"
-		for((integer = 1; integer <= ${headers_total}; integer++))
+	total_headers=`dpkg -l | grep linux-headers | awk '{print $2}' | grep -v "${latest_version}" | wc -l`
+	if [ "${total_headers}" > "1" ]; then
+		echo -e "${Info} find ${total_headers} other kernel, uninstalling"
+		for((integer = 1; integer <= ${total_headers}; integer++))
 		do
-			headers_del=`dpkg -l|grep linux-headers | awk '{print $2}' | grep -v "4.11.0" | head -${integer}`
-			echo -e "${Info} uninstalling ${headers_del}"
-			apt-get purge -y ${headers_del}
-			echo -e "${Success} uninstalling ${headers_del} successfully, continuing"
+			surplus_headers=`dpkg -l|grep linux-headers | awk '{print $2}' | grep -v "${latest_version}" | head -${integer}`
+			echo -e "${Info} uninstalling ${surplus_headers}"
+			apt-get purge -y ${surplus_headers}
+			echo -e "${Success} uninstall ${surplus_headers} successfully, continuing"
 		done
-		if [ "${headers_total}" = "0" ]; then
-			echo -e "${Success} uninstalling successfully, continuing"
+		if [ "${total_headers}" = "0" ]; then
+			echo -e "${Success} uninstall successfully, continuing"
 		else
-			echo -e "${Failed} uninstalling failed, please check!" && exit 1
+			echo -e "${Failed} uninstall failed, please check!" && exit 1
 		fi
 	else
-	echo -e "${Error} find a wrong kernel number, please check!" && exit 1
+	echo -e "${Error} check kernel failed, please check!" && exit 1
+	fi
+}
+
+#get latest address
+get_latest_version(){
+	echo -e "${Info} getting latest version"
+	latest_version=$(wget -qO- "http://kernel.ubuntu.com/~kernel-ppa/mainline/" | awk -F'\"v' '/v[4-9].[0-9]*.[0-9]/{print $2}' |grep -v '\-rc'| cut -d/ -f1 | sort -V | tail -1)
+	[[ -z ${latest_version} ]] && echo -e "${error} fail to get latest version !" && exit 1
+	echo -e "${Info} latest version is : ${Green_font}${latest_version}${Font_suffix}"
+}
+get_latest_kernel(){
+	bit=`uname -m`
+	if [[ ${bit} == "x86_64" ]]; then
+		image_name=$(wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${latest_version}/ | grep "linux-image" | grep "lowlatency" | awk -F'\">' '/amd64.deb/{print $2}' | cut -d'<' -f1 | head -1)
+		image_url="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${latest_version}/${image_name}"
+		headers_name_1=$(wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${latest_version}/ | grep "linux-headers" | grep "lowlatency" | awk -F'\">' '/amd64.deb/{print $2}' | cut -d'<' -f1 | head -1)
+		headers_url_1="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${latest_version}/${headers_name_1}"
+		headers_name_2=$(wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${latest_version}/ | grep "linux-headers" | awk -F'\">' '/all.deb/{print $2}' | cut -d'<' -f1 | head -1)
+		headers_url_2="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${latest_version}/${headers_name_2}"
+	elif [ ${bit} == "i386" ]; then
+		image_name=$(wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${latest_version}/ | grep "linux-image" | grep "lowlatency" | awk -F'\">' '/i386.deb/{print $2}' | cut -d'<' -f1 | head -1)
+		image_url="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${latest_version}/${image_name}"
+		headers_name_1=$(wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${latest_version}/ | grep "linux-headers" | grep "lowlatency" | awk -F'\">' '/i386.deb/{print $2}' | cut -d'<' -f1 | head -1)
+		headers_url_1="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${latest_version}/${headers_name_1}"
+		headers_name_2=$(wget -qO- http://kernel.ubuntu.com/~kernel-ppa/mainline/v${latest_version}/ | grep "linux-headers" | awk -F'\">' '/all.deb/{print $2}' | cut -d'<' -f1 | head -1)
+		headers_url_2="http://kernel.ubuntu.com/~kernel-ppa/mainline/v${latest_version}/${headers_name_2}"
+	else
+		echo -e "${Error} not support ${bit} !" && exit 1
 	fi
 }
 
 install_image(){
     mkdir /root/tcp_nanqinlang
     cd /root/tcp_nanqinlang
-	bit=`uname -m`
-	if [[ ${bit} == "x86_64" ]]; then
-         wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.11/linux-image-4.11.0-041100-lowlatency_4.11.0-041100.201705041534_amd64.deb	
-         if [ -s linux-image-4.11.0-041100-lowlatency_4.11.0-041100.201705041534_amd64.deb ]; then
-		     echo -e "${Success} download successfully, installing now"
-		     dpkg -i linux-image-4.11.0-041100-lowlatency_4.11.0-041100.201705041534_amd64.deb
-	     else
-		     echo -e "${Failed} download failed, please check!" && exit 1
-	     fi
-	elif [ ${bit} == "i386" ]; then
-    	 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.11/linux-image-4.11.0-041100-lowlatency_4.11.0-041100.201705041534_i386.deb	
-         if [ -s linux-image-4.11.0-041100-lowlatency_4.11.0-041100.201705041534_i386.deb ]; then
-		     echo -e "${Success} download successfully, installing now"
-		     dpkg -i linux-image-4.11.0-041100-lowlatency_4.11.0-041100.201705041534_i386.deb
-	     else
-		     echo -e "${Failed} download failed, please check!" && exit 1
-	     fi
+	wget ${image_url}
+	if [ -s ${image_name} ]; then
+		echo -e "${Success} image download successfully, installing"
+		dpkg -i ${image_name}
 	else
-	echo -e "${Error} not support ${bit} !" && exit 1
-	fi
+	echo -e "${Failed} image download failed, please check!" && exit 1
+    fi
 }
-
 install_headers(){
-    mkdir /root/tcp_nanqinlang
+	mkdir /root/tcp_nanqinlang
     cd /root/tcp_nanqinlang
-	wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.11/linux-headers-4.11.0-041100_4.11.0-041100.201705041534_all.deb
-	if [ -s linux-headers-4.11.0-041100_4.11.0-041100.201705041534_all.deb ]; then
-		echo -e "${Success} download successfully, installing now"
-		dpkg -i linux-headers-4.11.0-041100_4.11.0-041100.201705041534_all.deb
+	wget ${headers_url_1}
+	if [ -s ${headers_name_1} ]; then
+		echo -e "${Success} headers_bit download successfully, installing"
+		dpkg -i ${headers_name_1}
 	else
-		echo -e "${Failed} download failed, please check!" && exit 1
-	fi
-    bit=`uname -m`
-	if [[ ${bit} == "x86_64" ]]; then
-	     wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.11/linux-headers-4.11.0-041100-lowlatency_4.11.0-041100.201705041534_amd64.deb
-	     if [ -s linux-headers-4.11.0-041100-lowlatency_4.11.0-041100.201705041534_amd64.deb ]; then
-		     echo -e "${Success} download successfully, installing now"
-		     dpkg -i linux-headers-4.11.0-041100-lowlatency_4.11.0-041100.201705041534_amd64.deb
-	     else
-		     echo -e "${Failed} download failed, please check!" && exit 1
-	     fi
-	elif [ ${bit} == "i386" ]; then
-	     wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.11/linux-headers-4.11.0-041100-lowlatency_4.11.0-041100.201705041534_i386.deb
-	     if [ -s linux-headers-4.11.0-041100-lowlatency_4.11.0-041100.201705041534_i386.deb ]; then
-		     echo -e "${Success} download successfully, installing now"
-		     dpkg -i linux-headers-4.11.0-041100-lowlatency_4.11.0-041100.201705041534_i386.deb
-	     else
-		     echo -e "${Failed} download failed, please check!" && exit 1
-	     fi
+	echo -e "${Failed} headers_bit download failed, please check!" && exit 1
+    fi
+	
+	wget ${headers_url_2}
+	if [ -s ${headers_name_2} ]; then
+		echo -e "${Success} headers_all download successfully, installing"
+		dpkg -i ${headers_name_2}
 	else
-	echo -e "${Error} not support ${bit} !" && exit 1
-	fi
-}	
+	echo -e "${Failed} headers_all download failed, please check!" && exit 1
+    fi
+}
 
-#check running tcp_nanqinlang or not
-tcp_nanqinlang_status(){
-	check_tcp_nanqinlang_status_on=`sysctl net.ipv4.tcp_available_congestion_control | awk '{print $3}'`
-	if [ "${check_tcp_nanqinlang_status_on}" = "nanqinlang" ]; then
-	echo -e "${Info} tcp_nanqinlang is installed!"
-	    check_tcp_nanqinlang_status_off=`lsmod | grep nanqinlang`
-	    if [ "${check_tcp_nanqinlang_status_off}" = "" ]; then
-		    echo -e "${Failed} tcp_nanqinlang is installed but not running!"
-	    else
-		    echo -e "${Success} tcp_nanqinlang is running!"
-	    fi
+#check kernel
+check_kernel(){
+	get_latest_version
+	image=`dpkg -l|grep linux-image | awk '{print $2}' | awk -F '-' '{print $3}' | grep '[4-9].[0-9]*.'`
+	if [[ "${image}" != "" ]]; then
+		if [[ "${image}" == "${latest_version}" ]]; then
+			echo -e "${Info} image is already latest" && exit 0
+		else
+			echo -e "image is not latest, will ugrade your image" && install_image
+		fi
+	else
+	echo -e "${Error} check image failed, please check again!" && exit 1
+	fi
+	headers=`dpkg -l|grep linux-headers | awk '{print $2}' | awk -F '-' '{print $3}' | grep '[4-9].[0-9]*.'`
+	if [[ "${headers}" != "" ]]; then
+		if [[ "${headers}" == "${latest_version}" ]]; then
+			echo -e "${Info} headers is already latest" && exit 0
+		else
+			echo -e "headers is not latest, will ugrade your headers" && install_headers
+		fi
+	else
+	echo -e "${Error} check headers failed, please check again!" && exit 1
 	fi
 }
 
-#install tcp_nanqinlang
+#check status
+check_status(){
+	status_sysctl=`sysctl net.ipv4.tcp_available_congestion_control | awk '{print $3}'`
+	if [ "${status_sysctl}" = "nanqinlang" ]; then
+	echo -e "${Info} tcp_nanqinlang is installed!"
+	    status_lsmod=`lsmod | grep nanqinlang`
+	    if [ "${status_lsmod}" = "" ]; then
+		    echo -e "${Info} tcp_nanqinlang is installed but not running, please ${reboot} and run ${Start}"
+	    else
+		    echo -e "${Success} tcp_nanqinlang is running!" && exit 0
+	    fi
+	else
+	echo -e "${Error} tcp_nanqinlang not installed, please ${Install} and try start anain" && exit 1
+	fi
+}
+
+#install
 install_tcp_nanqinlang(){
 	
-	#check system is bebian/ubuntu or not
+	#run check system and determine it's bebian/ubuntu or not
 	check_system
 	if [[ ${release} != "debian" ]]; then
 		if [[ ${release} != "ubuntu" ]]; then
@@ -194,38 +211,32 @@ install_tcp_nanqinlang(){
 		echo -e "${Error} OpenVZ is not support!" && exit 1
     fi
 	
-    #determine it has 4.11.0 or not and remove surplus kernel
-	#kernel version = 4.11.0
-	image_ver_surplus=`dpkg -l | grep linux-image | awk '{print $2}' | awk -F '-' '{print $3}' | grep "4.11.0"`
-	headers_ver_surplus=`dpkg -l | grep linux-headers | awk '{print $2}' | awk -F '-' '{print $3}' | grep "4.11.0"`
-	#total digit of kernel without 4.11.0
-	image_total_surplus=`dpkg -l|grep linux-image | awk '{print $2}' | grep -v "4.11.0" | wc -l`
-	headers_total_surplus=`dpkg -l|grep linux-headers | awk '{print $2}' | grep -v "4.11.0" | wc -l`
-	if [ ${image_ver_surplus} == "4.11.0" ]; then
-	    echo -e "${Info} image 4.11.0 has been install"
-		     if [ ${image_total_surplus} != "0" ]; then
-			     echo -e "${Info} removing surplus image"
-			     del_image
-		     else 
-		         echo -e "${Info} no surplus image need to remove"
-		     fi
-	    else 
-	    echo -e "${Info} will install image 4.11.0"
-     	install_image
-	    fi
-
-	if [ ${headers_ver_surplus} == "4.11.0" ]; then
-		echo -e "${Info} headers 4.11.0 has been installed"
-		     if [ ${headers_total_surplus} != "0" ]; then
-			     echo -e "${Info} removing surplus headers"
-			     del_headers
-		     else 
-		         echo -e "${Info} no surplus headers need to remove"
-		     fi
-	    else 
-	    echo -e "${Info} will install headers 4.11.0"
-     	install_headers
-	    fi
+    #determine it is ${latest_version} or not and remove surplus kernel
+	get_latest_version
+    get_latest_kernel
+	#kernel version = ${latest_version}
+	surplus_ver_image="`dpkg -l | grep linux-image | awk '{print $2}' | awk -F '-' '{print $3}' | grep "${latest_version}"`"
+	surplus_ver_headers="`dpkg -l | grep linux-headers | awk '{print $2}' | awk -F '-' '{print $3}' | grep "${latest_version}"`"
+	#total digit of kernel without ${latest_version}
+	surplus_total_image="`dpkg -l|grep linux-image | awk '{print $2}' | grep -v "${latest_version}" | wc -l`"
+	surplus_total_headers="`dpkg -l|grep linux-headers | awk '{print $2}' | grep -v "${latest_version}" | wc -l`"
+	
+	if [ ${surplus_ver_image} == "${latest_version}" ]; then
+	echo -e "${Info} image already have a latest version"
+		   if [ ${surplus_total_image} != "0" ]; then
+		   echo -e "${Info} removing surplus image" && del_image
+		          if [ ${surplus_ver_headers} == "${latest_version}" ]; then
+				  echo -e "${Info} headers already have a latest version"
+				         if [ ${surplus_total_headers} != "0" ]; then
+						 echo -e "${Info} removing surplus headers" && del_headers
+						 else echo -e "${Info} no surplus headers need to remove"
+						 fi
+				  else echo -e "${Info} installing latest headers" && install_headers
+				  fi
+		   else echo -e "${Info} no surplus image need to remove"
+		   fi
+	else echo -e "${Info} installing latest image" && install_image
+	fi
 	
 	update-grub
 	
@@ -267,52 +278,56 @@ net.ipv4.tcp_congestion_control=nanqinlang\c" > /etc/sysctl.conf
 
 	sysctl -p
 	#reboot part
-	echo -e "${Careful} please remember reboot to apply kernel, and subsquently running this command: ${Start}"
-    exit
+	echo -e "${Careful} please remember after ${reboot}, run ${Start}"
+  exit 0
 }
 
 #start
 start_tcp_nanqinlang(){
-	check_deb
 	cd /root/tcp_nanqinlang
+	wget https://raw.githubusercontent.com/nanqinlang/tcp_nanqinlang/master/tcp_nanqinlang.c
+    echo -e "${Info} code is downloaded, making now"
+	apt-get update && apt-get install build-essential -y && apt-get update && apt-get install gcc-4.9 make git python -y && apt-get update
+	echo "obj-m:=tcp_nanqinlang.o" > Makefile
+    make -C /lib/modules/$(uname -r)/build M=`pwd` modules CC=/usr/bin/gcc-4.9
 	if [ -s tcp_nanqinlang.ko ]; then
-		echo -e "${Info} mod has been saved, loading now"
-		chmod +x ./tcp_nanqinlang.ko
-	    insmod tcp_nanqinlang.ko
-    else
-	    wget https://raw.githubusercontent.com/nanqinlang/tcp_nanqinlang/master/tcp_nanqinlang.ko
-		echo -e "${Info} mod is downloaded, loading now"
-		chmod +x ./tcp_nanqinlang.ko
-	    insmod tcp_nanqinlang.ko
+         echo -e "${Info} mod is made, loading now"
+	     chmod +x ./tcp_nanqinlang.ko
+		 insmod tcp_nanqinlang.ko
+         sysctl -p
+	else echo -e "${Failed} mod made failed, please check!" && exit 1
 	fi
-    sysctl -p
-	sleep 1s
-	tcp_nanqinlang_status
-	exit
-}
-
-#stop
-stop_tcp_nanqinlang(){
-	check_deb
-	sed -i '/net\.ipv4\.tcp_congestion_control=nanqinlang/d' /etc/sysctl.conf
-	rmmod tcp_nanqinlang.ko
-	sysctl -p
-	sleep 1s
-	echo -e "${Info} please remember reboot to stop tcp_nanqinlang"
-	exit
+    check_status
+	exit 0
 }
 
 #status
 status_tcp_nanqinlang(){
-	check_deb
-	tcp_nanqinlang_status
-	exit
+	check_status
+	exit 0
+}
+
+#upgrade
+upgrade_tcp_nanqinlang(){
+    get_latest_version
+    get_latest_kernel
+	check_kernel
+	exit 0
+}
+
+#stop
+stop_tcp_nanqinlang(){
+	sed -i '/net\.ipv4\.tcp_congestion_control=nanqinlang/d' /etc/sysctl.conf
+	sysctl -p
+	rm -rf /root/tcp_nanqinlang
+	echo -e "${Info} please remember ${reboot} to stop tcp_nanqinlang"
+	exit 0
 }
 
 action=$1
 [ -z $1 ] && action=install
 case "$action" in
-	install|start|stop|status)
+	install|start|status|upgrade|stop)
 	${action}_tcp_nanqinlang
     ${action}_tcp_nanqinlang 2>&1 | tee /root/tcp_nanqinlang.log
 	;;
